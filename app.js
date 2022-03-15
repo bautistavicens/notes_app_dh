f67f3b81bf510297212e965fb7d3ae135d7ae559
Bautista <bautivicens@gmail.com>
2022-03-14 21:37:45 -0300

    bugs solved.

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const listadorDeTareas = require("./listador_tareas");
 //Extrae las tareas del JSON.
 let tareas = jsonTranslate.leerJson();
 
-//Opcines de ingreso para el usuario.
-const opciones = ["Todas", "Terminadas", "Ejecucion", "Pendientes", "Cargar"];
+//Opcines de ingreso primer parametro.
+const opcionesPrincipales = ["Listar", "Cargar"];
+
+//Opcines de ingreso segundo parametro.
+const opcionesDeEstado = ["Todas", "Terminadas", "Ejecucion", "Pendientes"];
 
 //Entrada del usuario.
 let instruccionParam1 = process.argv[2];
@@ -15,31 +18,53 @@ let instruccionParam2 = process.argv[3];
 //Pasamos a mayusculas para comparar
 instruccionParam1 = instruccionParam1.toUpperCase();
 
-switch (instruccionParam1){
-    case "TODAS":
-        console.log("Tareas:");
-        listadorDeTareas.listar(tareas, instruccionParam1);
-         break;
-    case "TERMINADAS":
-        console.log("Tareas terminadas:");
-        listadorDeTareas.listar(tareas, instruccionParam1);
-        break;
-    case "EJECUCION":
-        console.log("Tareas en ejecución:");
-        listadorDeTareas.listar(tareas, instruccionParam1);
-        break;
-    case "PENDIENTES":
-        console.log("Tareas pendientes:");
-        listadorDeTareas.listar(tareas, instruccionParam1);
-        break;
-    case "CARGAR":
-        jsonTranslate.escribirJson(instruccionParam2);
-        break;
-    default:
-        console.log("\nOpción invalida.\nSeleccione una opción valida de listado:");
-        arrayPrinter.mostrarEnumerado(opciones);
-        break;
+//MAIN
+main();
+
+
+function main(){
+    switch(instruccionParam1){
+        case "LISTAR":
+            instruccionParam2 = instruccionParam2.toUpperCase();
+            leerPorEstados();
+            break;
+        case "CARGAR":
+            jsonTranslate.escribirJson(instruccionParam2);
+            break;
+        default:
+            console.log("\nOpción invalida.\nSeleccione una opción valida de listado:");
+            arrayPrinter.mostrarEnumerado(opcionesPrincipales);
+            break;
+
     }
+}
+
+
+function leerPorEstados(){
+    switch (instruccionParam2){
+        case "TODAS":
+            console.log("Tareas:");
+            listadorDeTareas.listar(tareas, instruccionParam2);
+            break;
+        case "TERMINADAS":
+            console.log("Tareas terminadas:");
+            listadorDeTareas.listar(tareas, instruccionParam2);
+            break;
+        case "EJECUCION":
+            console.log("Tareas en ejecución:");
+            listadorDeTareas.listar(tareas, instruccionParam2);
+            break;
+        case "PENDIENTES":
+            console.log("Tareas pendientes:");
+            listadorDeTareas.listar(tareas, instruccionParam2);
+            break;
+        default:
+            console.log("\nOpción invalida.\nSeleccione una opción valida de listado:");
+            arrayPrinter.mostrarEnumerado(opcionesDeEstado);
+            break;
+    }
+}
+
 
 
 
